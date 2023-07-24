94e8ff309c232e1113dc88ce1efae34bf9f9f67d
Andrey <a.e.batischev@gmail.com>
2023-07-24 22:11:36 +0300

    base routes

---
+++ server/src/category/entities/category.entity.ts
@@ -0,0 +1,33 @@
+import { Transaction } from 'src/transaction/entities/transaction.entity'
+import { User } from 'src/user/entities/user.entity'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'category_id' })
+  id: number
+
+  @Column()
+  title: string
+
+  @ManyToOne(() => User, (user) => user.categories)
+  @JoinColumn({ name: 'user_id' })
+  user: User
+
+  @ManyToOne(() => Transaction, (transaction) => transaction.category)
+  transactions: Transaction[]
+
+  @CreateDateColumn()
+  creasteAt: Date
+
+  @UpdateDateColumn()
+  updateAt: Date
+}
