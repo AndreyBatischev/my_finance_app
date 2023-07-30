83999f0ca7d990b45ee4f06841e616049062e9c9
Andrey <a.e.batischev@gmail.com>
2023-07-30 23:20:37 +0300

    base roytes front

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage: FC = () => {
-	return <div>ErrorPage</div>
+	return (
+		<div className="min-h-screen bg-slate-900 items-center font-roboto text-white flex justify-center flex-col gap-10">
+			<h1 className="text-9xl">404</h1>
+			<span className="text-4xl">Page not found</span>
+			<Link
+				to={'/'}
+				className="bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600"
+			>
+				Back
+			</Link>
+		</div>
+	)
 }
 
 export default ErrorPage
